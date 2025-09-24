64521cfde3a165c6926c40b53d230053408a2e40
elammaryo <122314668+elammaryo@users.noreply.github.com>
2025-09-23 21:05:29 -0400

    integrate AI responses + api call

---
+++ api.js
@@ -0,0 +1,24 @@
+const baseURL = "http://localhost:3000";
+
+export async function getAiMessage({ inputMessage }) {
+  try {
+    const response = await fetch(`${baseURL}/chatbot/aiChatResponse`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        role: "user",
+        content: inputMessage,
+      }),
+    });
+
+    const data = (await response.json()) ?? [];
+
+    return data.content;
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+window.getAiMessage = getAiMessage;
